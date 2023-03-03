7b7285b85e1f6f2a07ee930354cfe84ea902fd73
msereda07 <msereda07@gmail.com>
2023-03-03 21:04:35 +0300

    edited text

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ const qr = document.querySelector(".qr_code");
 const block =document.querySelector(".promo_TikTok");
 function togle(){
     qr.classList.toggle("hidden");
+    block.classList.toggle("Tt_close");
 }
 block.addEventListener("click", togle);
