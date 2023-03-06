010d7f52a399480e77177331f05fb8154697a13a
msereda07 <msereda07@gmail.com>
2023-03-07 02:18:03 +0300

    animations

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const qr = document.querySelector(".qr_code");
 const block =document.querySelector(".promo_TikTok");
-const arrow =  document.querySelector(".tt_arrow")
+const arrow =  document.querySelector(".tt_arrow");
 function togle(){
     qr.classList.toggle("hidden");
     block.classList.toggle("Tt_close");
+    arrow.classList.toggle("arrow_close");
 }
 block.addEventListener("click", togle);
